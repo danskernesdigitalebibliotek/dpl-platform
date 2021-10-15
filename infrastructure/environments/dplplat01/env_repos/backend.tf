b48eafcf55802e06d9ec45fe835f48f061846224
Mads H. Danquah <mads@danquah.dk>
2021-10-15 22:54:14 +0200

    Add a root module and site-config for dplplat01 env repos
    DDFDPDEL-180

---
+++ infrastructure/environments/dplplat01/env_repos/backend.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "stdpltfstatealpha001"
+    container_name       = "state"
+    key                  = "dplplat01_github.tfstate"
+  }
+
+  required_providers {
+    github = {
+      source = "integrations/github"
+    }
+  }
+}
+
+provider "github" {
+  # Personal Access Token is assumed to have been made available via the
+  # GITHUB_TOKEN environment variable and thus is not configured here.
+
+  # The provider needs to be configured with the organization that we're
+  # creating repositories in.
+  owner = "danishpubliclibraries"
+}
+
