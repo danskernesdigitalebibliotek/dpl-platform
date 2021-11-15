1d0e3b6d1d8e86216b97d35f3cbafd7e92db4590
Mads H. Danquah <mads@danquah.dk>
2021-11-15 11:32:53 +0100

    Install the K8up operator into the cluster
    This commit installs the K8up operator that Lagoon relies on for backup.
    
    The values for the operator follows the example documented in the
    lagoon documentation (https://gist.github.com/Schnitzel/5b87a9e9ee7c59b2bc6b29f0f0839d56)
    and adds credentials for the previously provisioned resources.

---
+++ infrastructure/task/scripts/provision-k8up.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+#
+# Install and configure two k8up gateways for Azure blob storage
+#
+# Requires a functional and authenticated kubectl context.
+set -euo pipefail
+
+IFS=$'\n\t'
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# shellcheck source=./util.source.sh
+source "${SCRIPT_DIR}/util.source.sh"
+
+# Determine the platform environment.
+if [[ -z "${DPLPLAT_ENV:-}" ]] ; then
+    echo "Missing DPLPLAT_ENV environment variable"
+    exit 1
+fi
+
+# Pull in the environment-specific versions and verify we have what we need.
+# shellcheck source=/dev/null
+source "$(getVersionsEnv)"
+
+# These variables are either pulled in via the environment or versions.env.
+# The credentials the client (eg. lagoon) has to present to k8up to gain access.
+verifyVariable "API_URL"
+verifyVariable "CLIENT_ACCESS_KEY"
+verifyVariable "CLIENT_SECRET_KEY"
+verifyVariable "K8UP_GLOBALSTATSURL"
+verifyVariable "LAGOON_BACKUP_HANDLER_URL"
+
+# Setup values we will use troughout the scripts
+diff_or_nothing=$(ifDiffTernary "diff")
+configuration_dir=$(getConfigurationDir)
+apply_or_diff=$(ifDiffTernary "diff" "apply" )
+
+# Proceede to provisioning.
+
+# Setup the namespace.
+set +e
+kubectl "${apply_or_diff}" -f "${configuration_dir}/k8up/namespace.yaml"
+handleApplyDiffExit $?
+set -e
+
+# Install the Helm repo, we'll need this regardless of whether we're diffing.
+setupHelmRepo appuio https://charts.appuio.ch
+
+# shellcheck disable=SC2016
+envsubst '$API_URL $K8UP_GLOBALSTATSURL $CLIENT_ACCESS_KEY $CLIENT_SECRET_KEY $LAGOON_BACKUP_HANDLER_URL' \
+  < "${configuration_dir}/k8up/k8up-values.template.yaml" \
+  > "${configuration_dir}/k8up/k8up-values.yaml"
+
+isDiffing && echo " > Diffing release"
+kubectl "${apply_or_diff}" -f "https://github.com/vshn/k8up/releases/download/v1.2.0/k8up-crd.yaml"
+# shellcheck disable=SC2086 # We need diff_or_nothing to be unquoted
+helm ${diff_or_nothing} upgrade --install \
+  k8up appuio/k8up \
+  --namespace k8up \
+  --version "${VERSION_K8UP}" \
+  --values "${configuration_dir}/k8up/k8up-values.yaml"
+
+echo " > Done."
+
+
