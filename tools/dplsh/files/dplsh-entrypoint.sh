0ac3ba965abf51f3550d4e4f90240c39ece1ca69
Mads H. Danquah <mads@danquah.dk>
2023-02-28 21:43:23 +0000

    Fix the ownership of the files the entrypoint rsyncs into the container.
    This required a change where the entrypoint now runs as root, but drops privilegeds when it is done with its work.

--- tools/dplsh/files/dplsh-entrypoint.sh
+++ tools/dplsh/files/dplsh-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+#
+# The entrypoint is the first thing that runs when the container is started by
+# dplsh.sh. It is responsible for doing the last minute setup that could not
+# be done in the Dockerfile, and then start the dplsh users shell.
+#
+# Be aware that the entrypoint runs as root, but ends with a su to the dplsh
+# user to ensure the user ends in an unprivileged shell.
+#
 set -euo pipefail
 
 # Output a script that itself will start dplsh via docker.
@@ -12,29 +20,40 @@ fi
 # the host.
 if [[ -d /home/dplsh/.azure-host ]] ; then
     mkdir -p /home/dplsh/.azure
-    rsync -a --exclude=logs --exclude=commands --exclude=telemetry /home/dplsh/.azure-host/ /home/dplsh/.azure/
+    rsync \
+      --archive \
+      --chown dplsh:dplsh \
+      --exclude=logs \
+      --exclude=commands \
+      --exclude=telemetry \
+      --exclude=.git \
+      /home/dplsh/.azure-host/ /home/dplsh/.azure/
 fi
 
 if [[ -f /home/dplsh/.gitconfig-host ]] ; then
     cp /home/dplsh/.gitconfig-host /home/dplsh/.gitconfig
+    chown dplsh /home/dplsh/.gitconfig
 fi
 
 if [[ -d /home/dplsh/.ssh-host ]] ; then
     mkdir -p /home/dplsh/.ssh
-    rsync -a --exclude=commands /home/dplsh/.ssh-host/ /home/dplsh/.ssh/
+    rsync --archive \
+      --chown dplsh:dplsh \
+      --exclude=commands \
+      --exclude=.git \
+      /home/dplsh/.ssh-host/ /home/dplsh/.ssh/
 fi
 
 PATH=${PATH}:${HOME}/bin
 export PATH
 
-# See if dplsh was passed an argument.
+# Start the unprivileged shell, passing on any additional arguments we got.
 if [[ $# -gt 0 ]] ; then
     # We got aguments, eg the user invoked
     # dplsh ls -l
     # fire up a login-shell and execute the command
-    bash -l -c "${*}"
+    exec su dplsh -c "bash -l -c '${*}'"
 else
     # No argument, just start a login shell.
-    bash -l
+    exec su dplsh -c "bash -l"
 fi
-
