467de41e9cb4349b8044bf6997f000891dc43c81
Philip <philip@deranged.dk>
2024-04-19 09:50:35 +0200

    terraform provision comms services and email service

---
+++ infrastructure/terraform/modules/dpl-platform-environment/communication_services.tf
@@ -0,0 +1,24 @@
+# resource "azurerm_key_vault_secret" "azurerm_communication_service_connection_string" {
+#   name         = "azurerm-communication-service-connection-string"
+#   value        = azurerm_communication_service.acs.primary_connection_string
+#   key_vault_id = azurerm_key_vault.keyvault.id
+# }
+provider "azurerm" {
+  features {}
+}
+
+resource "random_id" "acs" {
+  byte_length = 2
+}
+
+resource "azurerm_communication_service" "communications-services" {
+  name                = "communication-services${random_id.acs.hex}"
+  resource_group_name = azurerm_resource_group.rg.name
+  data_location       = "Germany"
+}
+
+resource "azurerm_email_communication_service" "email-service" {
+  name                = "email-service${random_id.acs.hex}"
+  resource_group_name = azurerm_resource_group.rg.name
+  data_location       = "Germany"
+}
