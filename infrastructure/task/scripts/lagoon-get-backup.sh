49e3c476819eacb73f763ed0af4296427424edef
Kasper Garnæs <kasperg@users.noreply.github.com>
2025-01-21 08:50:41 +0100

    Update Lagoon backup retrieval script for DPLSH
    The lagoon get backup CLI command returns a string where the backup
    url is prefixed by the word "Result:". This is likely caused by the
    version of the Lagoon CLI used in DPLSH.
    
    Switch to JSON output and extract the URL with jq instead.

--- infrastructure/task/scripts/lagoon-get-backup.sh
+++ infrastructure/task/scripts/lagoon-get-backup.sh
@@ -11,16 +11,17 @@ fi
 
 BACKUPS=$(lagoon list backups -p "${LAGOON_PROJECT}" -e "${LAGOON_ENVIRONMENT}" --output-json);
 BACKUP_ID=$(echo "$BACKUPS" | jq -r ".data[] | select(.source == \"${BACKUP_TYPE}\") | .backupid" | head -n 1);
-BACKUP_URL="Error";
+BACKUP_RESULT="Error";
 echo -e "\nRetrieving ${BACKUP_TYPE} backup from ${LAGOON_ENVIRONMENT} environment of ${LAGOON_PROJECT} project \n\n";
 # Wait a while we wait for the backup to become available.
 # It must be retrieved before it can be downloaded.
-while [[ $BACKUP_URL == "Error"* ]]; do
+while [[ $BACKUP_RESULT == "Error"* ]]; do
   echo -n ".";
   eval "lagoon retrieve backup -p \"${LAGOON_PROJECT}\" -e \"${LAGOON_ENVIRONMENT}\" --backup-id \"${BACKUP_ID}\" --force &> /dev/null" || true;
   # This will return an message in the format "Error: [error message]" if the
   # backup is not available for download yet.
-  BACKUP_URL=$(lagoon get backup -p "${LAGOON_PROJECT}" -e "${LAGOON_ENVIRONMENT}" --backup-id "${BACKUP_ID}" 2>/dev/null) || true;
+  BACKUP_RESULT=$(lagoon get backup -p "${LAGOON_PROJECT}" -e "${LAGOON_ENVIRONMENT}" --backup-id "${BACKUP_ID}" --output-json 2>/dev/null) || true;
+  BACKUP_URL=$(echo "$BACKUP_RESULT" | jq -r ".result") || true
 done;
 echo -e "\nDownloading backup from ${BACKUP_URL} to ${BACKUP_DESTINATION}\n\n";
 curl -o "${BACKUP_DESTINATION}" "${BACKUP_URL}";
