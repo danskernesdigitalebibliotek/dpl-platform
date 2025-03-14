94968bb47f5dda02ea06780355207835feb16307
ITViking <philip.dein@proton.me>
2025-03-14 13:36:23 +0100

    split redployDeployments function into two parts for readability

--- infrastructure/images/lagoon-redeploy-controller/redeploy.sh
+++ infrastructure/images/lagoon-redeploy-controller/redeploy.sh
@@ -19,67 +19,39 @@
 # Set dplplat01 as default config
 /lagoon config default --lagoon dplplat01
 
+getFailedDeployments() {
+  local environment_type=$1
+  /lagoon raw --raw "query allProjects {
+    allProjects {
+      name
+      environments(type: $environment_type) {
+        name
+        deployments(limit: 1) {
+          status
+          created
+        }
+      }
+    }
+  }" | jq -r '.allProjects[] | .name as $name | .environments[].deployments[] | select(.status == "failed") | ($name)'
+}
+
 redeployDeployments() {
-  date '+%H:%M'
-  FAILED_PRODUCTION_DEPLOYMENTS=$(/lagoon raw --raw "query allProjects {
-    allProjects {
-    name
-      environments(type: PRODUCTION) {
-        name
-        deployments(limit: 1) {
-          status
-          created
-        }
-      }
-    }
-  }" | jq -r '.allProjects[] | .name as $name | .environments[].deployments[] | select(.status == "failed") | ($name)')
+  local environment_type=$1
+  local environment_name=$2
+  local failed_deployments=$(getFailedDeployments $environment_type)
 
-  if [ -n "$FAILED_PRODUCTION_DEPLOYMENTS" ]; then
-    echo "Redeploying failed production environments"
+  if [ -n "$failed_deployments" ]; then
+    echo "Redeploying failed $environment_type environments"
 
-    for deployment in $FAILED_PRODUCTION_DEPLOYMENTS; do
-      if [[ $deployment =~ "dpl-cms" ]]; then
-        continue
-      fi
-      if [[ $deployment =~ "dpl-bnf" ]]; then
+    for deployment in $failed_deployments; do
+      if [[ $deployment =~ "dpl-cms" || $deployment =~ "dpl-bnf" ]]; then
         continue
       fi
       echo "$deployment: deploying"
-      /lagoon deploy latest -p "$deployment" -e "main" --force
+      /lagoon deploy latest -p "$deployment" -e "$environment_name" --force
     done
   else
-    echo "No failed production deployments to redeploy"
-  fi
-
-  echo "Now checking for failed development site deployments"
-  FAILED_DEVELOPMENT_DEPLOYMENTS=$(/lagoon raw --raw "query allProjects {
-    allProjects {
-    name
-      environments(type: DEVELOPMENT) {
-        name
-        deployments(limit: 1) {
-          status
-          created
-        }
-      }
-    }
-  }" | jq -r '.allProjects[] | .name as $name | .environments[].deployments[] | select(.status == "failed") | ($name)')
-
-  if [ -n "$FAILED_DEVELOPMENT_DEPLOYMENTS" ]; then
-    echo "Redeploying failed development environments"
-
-    for deployment in $FAILED_DEVELOPMENT_DEPLOYMENTS; do
-      if [[ $deployment =~ "dpl-cms" ]]; then
-        continue
-      fi
-      if [[ $deployment =~ "dpl-bnf" ]]; then
-        continue
-      fi
-      echo "$deployment: deploying"
-      /lagoon deploy latest -p "$deployment" -e "moduletest" --force
-    done
-  else
-    echo "No failed development deployments to redeploy"
+    echo "No failed $environment_type deployments to redeploy"
   fi
 }
 
