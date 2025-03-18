007cd17ed12897e778f2a8a9701c2e7b1e61d231
ITViking <philip.dein@proton.me>
2025-03-18 16:04:50 +0100

    add zx redeploy-controller. It is not on par yet, but I have arrived at a point where it needs access to some volumes to run

---
+++ infrastructure/images/lagoon-redeploy-controller/redeploy-controller.mjs
@@ -0,0 +1,48 @@
+#!/usr/bin/env zx
+useBash();
+
+$.sync`chmod 600 /root/.ssh/id_rsa`
+
+await $`lagoon config add \
+  --graphql https://api.lagoon.dplplat01.dpl.reload.dk/graphql \
+  --force \
+  --ui https://ui.lagoon.dplplat01.dpl.reload.dk \
+  --hostname 20.238.147.183 \
+  --port 22 \
+  --lagoon dplplat01`
+
+await $`lagoon config default --lagoon dplplat01`
+
+// verify that dplplat01 is the active lagoon
+echo(await $`lagoon config list`)
+
+function getFailedDeployments(environmentType) {
+  const deployments = $.sync`lagoon raw --raw "query allProjects {
+    allProjects {
+      name
+      environments(type: ${environmentType}) {
+        name
+        deployments(limit: 1) {
+          status
+          created
+        }
+      }
+    }
+  }"`;
+
+  return deployments;
+}
+
+function redeployDeployments(environmentType, environmentName, allowRedeployAttemps) {
+  const failedDeployments = getFailedDeployments(environmentType);
+  console.log(failedDeployments);
+}
+
+const wait = ms => new Promise(res => setTimeout(res, ms));
+
+while(true) {
+  redeployDeployments("PRODUCTION", "main", 6);
+  redeployDeployments("DEVELOPMENT", "moduletest", 3);
+  echo("sleeping for 5 minutes before redeploying again");
+  await wait(300000);
+}
