b4713dd6d8299610d2c04e5cf88affc6beab48c8
ITViking <philip.dein@proton.me>
2025-03-26 11:48:34 +0100

    log out what we're going to do

--- infrastructure/images/moduletest-reset-jobs/moduletest-files-reset-job.mjs
+++ infrastructure/images/moduletest-reset-jobs/moduletest-files-reset-job.mjs
@@ -5,3 +5,9 @@ if (!projectName) {
   throw Error("No 'projectName' provided");
 }
 
+console.log(`Reseting files from ${projectName}-main to ${projectName}-moduletest
+  \n
+  Will now delete all files and folder in '/app/web/sites/default/files'
+  \n
+`);
+
