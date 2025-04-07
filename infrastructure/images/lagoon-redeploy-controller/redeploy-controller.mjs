3b41365109f745b8ffbb836e556faf1ec5550dec
ITViking <philip.dein@proton.me>
2025-04-07 09:48:34 +0200

    only select those that have failed, so we don't redeploy queued or cancelled deployments

--- infrastructure/images/lagoon-redeploy-controller/redeploy-controller.mjs
+++ infrastructure/images/lagoon-redeploy-controller/redeploy-controller.mjs
@@ -37,7 +37,7 @@ function getFailedDeployments(environmentType) {
         }
       }
     }
-  }" | jq -r '.allProjects[] | .name as $name | .environments[].deployments[] | select(.status != "complete") | ($name)'`.valueOf().split("\n");
+  }" | jq -r '.allProjects[] | .name as $name | .environments[].deployments[] | select(.status == "failed") | ($name)'`.valueOf().split("\n");
 }
 
 const redeployedDeployments = {};
