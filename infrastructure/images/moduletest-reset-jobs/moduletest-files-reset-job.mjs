c6ba1be4004b6ec548df996b497d8dfff3968fc2
ITViking <philip.dein@proton.me>
2025-04-07 10:19:56 +0200

    Start by trying to use the cli containers Laggon CLI so we can get access to rsync for fast file moving

--- infrastructure/images/moduletest-reset-jobs/moduletest-files-reset-job.mjs
+++ infrastructure/images/moduletest-reset-jobs/moduletest-files-reset-job.mjs
@@ -27,7 +27,8 @@ try {
 echo(`Will now move files from ${projectName}-main to ${projectName}-moduletest`);
 
 try {
-  await $` kubectl exec -n ${projectName}-main deploy/cli -- tar cf - /app/web/sites/default/files | kubectl exec -i -n ${projectName}-moduletest deploy/cli -- tar xvf - -C /`;
+  await $`kubectl exec -n ${projectName}-moduletest -- bash -c "drush -y rsync @lagoon.${projectName}-main:%files @self:%files -- --omit-dir-times --no-perms --no-group --no-owner --no-times --chmod=ugo=rwX --delete --exclude=css/* --exclude=js/* --exclude=styles/* --delete-excluded"`
+  // await $`kubectl exec -n ${projectName}-main deploy/cli -- tar cf - /app/web/sites/default/files | kubectl exec -i -n ${projectName}-moduletest deploy/cli -- tar xvf - -C /`;
 } catch(error) {
   throw Error("file move failed", error.stderr);
 }
