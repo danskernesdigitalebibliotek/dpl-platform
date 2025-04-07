2445b348d52ea4acc02a4d4360b01c1a16937c15
ITViking <philip.dein@proton.me>
2025-04-07 10:21:59 +0200

    write error out and shorten completion message

--- infrastructure/images/moduletest-reset-jobs/moduletest-files-reset-job.mjs
+++ infrastructure/images/moduletest-reset-jobs/moduletest-files-reset-job.mjs
@@ -30,7 +30,8 @@ try {
   await $`kubectl exec -n ${projectName}-moduletest -- bash -c "drush -y rsync @lagoon.${projectName}-main:%files @self:%files -- --omit-dir-times --no-perms --no-group --no-owner --no-times --chmod=ugo=rwX --delete --exclude=css/* --exclude=js/* --exclude=styles/* --delete-excluded"`
   // await $`kubectl exec -n ${projectName}-main deploy/cli -- tar cf - /app/web/sites/default/files | kubectl exec -i -n ${projectName}-moduletest deploy/cli -- tar xvf - -C /`;
 } catch(error) {
+  echo("The file move failed badly", error.stderr);
   throw Error("file move failed", error.stderr);
 }
 
-echo(`File reset for ${projectName}-moduletest complete`);
+echo(`File reset for ${projectName} complete`);
