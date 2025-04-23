a3fe69edf2fd82935d8ff72dd0c70bb200502aa8
ITViking <philip.dein@proton.me>
2025-04-23 20:47:37 +0200

    define shell type to run in and explan what the script does

---
+++ infrastructure/task/scripts/create-go-and-bnf-consumer-and-secrets.mjs
@@ -0,0 +1,4 @@
+#!/usr/bin/env zx
+// This script set secrets and consumers for DPL CMS projects. The secrets and consumers are to be identical by secret across all projects.
+// source: https://reload.zulipchat.com/#narrow/channel/240325-DDF/topic/Sammenkobling.20af.20DT.20og.20Go.20demo/near/511245805
+
