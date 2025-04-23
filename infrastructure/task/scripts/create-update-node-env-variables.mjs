9fc4e58d32b0320439d2f9b3aa8f3869fcd57310
ITViking <philip.dein@proton.me>
2025-04-23 21:37:48 +0200

    tell the user what the script will now do

--- infrastructure/task/scripts/create-update-node-env-variables.mjs
+++ infrastructure/task/scripts/create-update-node-env-variables.mjs
@@ -1,2 +1,5 @@
 #!/usr/bin/env zx
 // This script set variables with secrets for the node service to use
+
+echo("Creating og updating node service reliant variables for all environments");
+echo("");
