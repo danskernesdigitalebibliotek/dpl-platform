65ff7c84dd649b9046aea7d3dbfcbd2e0c255f8d
ITViking <philip.dein@proton.me>
2025-04-23 21:38:16 +0200

    get all sites so we can loop over them and define list of variable to create or update

--- infrastructure/task/scripts/create-update-node-env-variables.mjs
+++ infrastructure/task/scripts/create-update-node-env-variables.mjs
@@ -3,3 +3,21 @@
 
 echo("Creating og updating node service reliant variables for all environments");
 echo("");
+const sites = await $`cat ../host_mount/environments/dplplat01/sites.yaml | yq '.sites | ... comments="" | keys | .[]'`;
+
+const lagoonVariableName = [
+  "GO_SESSION_SECRET",
+  "NEXT_PUBLIC_GRAPHQL_BASIC_TOKEN_DPL_CMS",
+  "NEXT_PUBLIC_GRAPHQL_SCHEMA_ENDPOINT_DPL_CMS",
+  "NEXT_PUBLIC_GRAPHQL_SCHEMA_ENDPOINT_FBI",
+  "UNILOGIN_CLIENT_ID",
+  "UNILOGIN_CLIENT_SECRET",
+  "UNILOGIN_MUNICIPALITY_ID",
+  "UNILOGIN_WELLKNOWN_URL",
+  "UNLILOGIN_PUBHUB_CLIENT_ID",
+  "UNLILOGIN_PUBHUB_RETAILER_ID",
+  "UNLILOGIN_PUBHUB_RETAILER_KEY_CODE",
+  "UNLILOGIN_SERVICES_WS_PASSWORD",
+  "UNLILOGIN_SERVICES_WS_USER"
+];
+
