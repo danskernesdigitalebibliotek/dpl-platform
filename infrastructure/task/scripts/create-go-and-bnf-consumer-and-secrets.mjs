89ac9d2d1c4fd776eb6e5183283b036169442bd0
ITViking <philip.dein@proton.me>
2025-04-23 20:48:13 +0200

    let the user know the script/task is running

--- infrastructure/task/scripts/create-go-and-bnf-consumer-and-secrets.mjs
+++ infrastructure/task/scripts/create-go-and-bnf-consumer-and-secrets.mjs
@@ -2,3 +2,5 @@
 // This script set secrets and consumers for DPL CMS projects. The secrets and consumers are to be identical by secret across all projects.
 // source: https://reload.zulipchat.com/#narrow/channel/240325-DDF/topic/Sammenkobling.20af.20DT.20og.20Go.20demo/near/511245805
 
+echo("Setting BNF and GO consumer secrets and passwords on all environments across projects");
+echo("");
