4859a47c98fa72cd14b09813967f5eb5686ac46f
ITViking <philip.dein@proton.me>
2025-04-23 20:50:17 +0200

    import crypto lib for use with generating the secrets

--- infrastructure/task/scripts/create-go-and-bnf-consumer-and-secrets.mjs
+++ infrastructure/task/scripts/create-go-and-bnf-consumer-and-secrets.mjs
@@ -2,6 +2,8 @@
 // This script set secrets and consumers for DPL CMS projects. The secrets and consumers are to be identical by secret across all projects.
 // source: https://reload.zulipchat.com/#narrow/channel/240325-DDF/topic/Sammenkobling.20af.20DT.20og.20Go.20demo/near/511245805
 
+import * as crypto from "crypto";
+
 echo("Setting BNF and GO consumer secrets and passwords on all environments across projects");
 echo("");
 const sites = await $`cat ../host_mount/environments/dplplat01/sites.yaml | yq '.sites | ... comments="" | keys | .[]'`;
