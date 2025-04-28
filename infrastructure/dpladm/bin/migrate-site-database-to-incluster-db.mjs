72a34c91192229f13a7368e91d3dc67f5a3e3b6b
ITViking <philip.dein@proton.me>
2025-04-28 11:00:26 +0200

    create the database along with it's user through helm

--- infrastructure/dpladm/bin/migrate-site-database-to-incluster-db.mjs
+++ infrastructure/dpladm/bin/migrate-site-database-to-incluster-db.mjs
@@ -19,3 +19,16 @@ if (typeof `${process.argv[5]}` === "undefined") {
 
 const password = crypto.randomBytes(32).toString("base64");
 
+await createDatabaseGrantUserSecret(project, environment, password, dryRun);
+async function createDatabaseGrantUserSecret(project, environment, password, dryRun) {
+  echo(`Now migrating ${project}-${environment} database to incluser database`)
+
+  echo(await $`helm upgrade --install --namespace ${project}-${environment}  --set password=${password}  mariadb-database ./dpladm/mariadb-database/ --dry-run`);
+
+  if (dryRun === false) {
+    echo("done")
+    await $`exit 1`;
+  }
+
+  await $`helm upgrade --install --namespace ${project}-${environment}  --set password=${password}  mariadb-database ./dpladm/mariadb-database/`;
+}
