4f564a27dd505dc83de3e405b8c25762e95be3a3
ITViking <philip.dein@proton.me>
2025-04-28 10:58:29 +0200

    the script requires project and environemnt to work, so exit if they are not there

--- infrastructure/dpladm/bin/migrate-site-database-to-incluster-db.mjs
+++ infrastructure/dpladm/bin/migrate-site-database-to-incluster-db.mjs
@@ -1,2 +1,13 @@
 #!/usr/bin/env zx
 
+
+const project = `${process.argv[3]}`;
+if (!project) {
+  throw Error("No 'project' provided");
+}
+
+const environment = `${process.argv[4]}`;
+if (!environment) {
+  throw Error("No 'environment' provided");
+}
+
