48bcd781e4a389ae5bf66c40235912a71161403e
ITViking <philip.dein@proton.me>
2025-05-06 15:44:30 +0200

    should've been templated so we acutally get the project name out in the log

--- infrastructure/images/moduletest-reset-job/moduletest-reset-job.mjs
+++ infrastructure/images/moduletest-reset-job/moduletest-reset-job.mjs
@@ -8,10 +8,10 @@ if (!projectName) {
 echo(`Will now sync databases from ${projectName}-main to ${projectName}-moduletest`);
 
 try {
-  await $`kubectl exec -n ${projectName}-moduletest deployment/cli -- bash -c "drush sql-drop -y; drush -y sql-sync @lagoon.${projectName}-main @self --create-db"`
+  await $`kubectl exec -n ${projectName}-moduletest deployment/cli -- bash -c 'drush sql-drop -y; drush -y sql-sync @lagoon.${projectName}-main @self --create-db'`
 } catch(error) {
-  echo("Database sync for ${projectName} moduletest failed", error.stderr);
-  throw Error("Database sync for ${projectName} moduletest failed", { cause: error });
+  echo(`Database sync for ${projectName} moduletest failed`, error.stderr);
+  throw Error(`Database sync for ${projectName} moduletest failed`, { cause: error });
 }
 
 echo(`Database reset for ${projectName} complete`);
@@ -40,8 +40,8 @@ echo(`Now moving files from ${projectName}-main to ${projectName}-moduletest`);
 try {
   await $`kubectl exec -n ${projectName}-moduletest deployment/cli -- bash -c "drush -y rsync @lagoon.${projectName}-main:%files @self:%files -- --omit-dir-times --no-perms --no-group --no-owner --no-times --chmod=ugo=rwX --delete --exclude=css/* --exclude=js/* --exclude=styles/* --delete-excluded"`
 } catch(error) {
-  echo("The file move failed for ${projectName} moduletest", error.stderr);
-  throw Error("The file move failed for ${projectName} moduletest", { cause: error });
+  echo(`The file move failed for ${projectName} moduletest`, error.stderr);
+  throw Error(`The file move failed for ${projectName} moduletest`, { cause: error });
 }
 
 
@@ -49,8 +49,8 @@ echo(`Starting 'drush deploy' in ${projectName}-moduletest`);
 try {
   await $`kubectl exec -n ${projectName}-moduletest deployment/cli -- bash -c "drush deploy"`
 } catch(error) {
-  echo("The file move failed for ${projectName} moduletest", error.stderr);
-  throw Error("The file move failed for ${projectName} moduletest", { cause: error });
+  echo(`The file move failed for ${projectName} moduletest`, error.stderr);
+  throw Error(`The file move failed for ${projectName} moduletest`, { cause: error });
 }
 
 echo(`File reset for ${projectName} complete`);
