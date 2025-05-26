953d9df8e22aacc0c409b6ea3276abc4584172ea
ITViking <philip.dein@proton.me>
2025-05-26 13:22:03 +0200

    first get the source and targets database connection information

--- infrastructure/images/moduletest-reset-job/moduletest-reset-job.mjs
+++ infrastructure/images/moduletest-reset-job/moduletest-reset-job.mjs
@@ -7,6 +7,10 @@ if (!projectName) {
 
 const azureDatabaseHost = $.env.AZURE_DATABASE_HOST;
 
+const sourceNamespace = projectName + "-main";
+const sourceDatabaseConnectionInfo = await getDatabaseConnectionInfo(sourceNamespace);
+const targetNamespace = projectName + "-moduletest";
+const targetDatabaseConnectionInfo = await getDatabaseConnectionInfo(targetNamespace);
 
 
   }
@@ -14,6 +18,18 @@ const azureDatabaseHost = $.env.AZURE_DATABASE_HOST;
 
 }
 
+async function getDatabaseConnectionInfo(namespace) {
+  echo(`Getting ${namespace}'s database connection details`);
+  let configMapJson;
+  try {
+    configMapJson = await $`kubectl get -n ${namespace} configmap lagoon-env -o json`
+  } catch(error) {
+    echo(`Failed to get configmap "lagoon-env" in namespace ${namespace}`, error.stderr);
+    throw Error(`Failed to get configmap "lagoon-env" in namespace ${namespace}`, { cause: error });
+  }
+
+  const configmap = JSON.parse(configMapJson);
+  const { data } = configmap;
 
 echo(`Starting 'drush deploy' in ${projectName}-moduletest`);
 try {
@@ -21,6 +37,28 @@ try {
 } catch(error) {
   echo("The file move failed for ${projectName} moduletest", error.stderr);
   throw Error("The file move failed for ${projectName} moduletest", { cause: error });
+  let databaseConnectionInfo = {
+    databaseName: "",
+    databaseHost: "",
+    databaseUser: "",
+    databasePassword : "",
+  };
+
+  if(data.OVERRIDE_MARIADB_DATABASE != undefined) {
+    databaseConnectionInfo.databaseName = data.OVERRIDE_MARIADB_DATABASE;
+    databaseConnectionInfo.databaseHost = data.OVERRIDE_MARIADB_HOST;
+    databaseConnectionInfo.databasePassword = data.OVERRIDE_MARIADB_PASSWORD;
+    databaseConnectionInfo.databaseUser = data.OVERRIDE_MARIADB_USERNAME;
+    databaseConnectionInfo.override = true;
+  } else {
+    databaseConnectionInfo.databaseName = data.MARIADB_DATABASE;
+    databaseConnectionInfo.databaseHost = data.MARIADB_HOST;
+    databaseConnectionInfo.databasePassword = data.MARIADB_PASSWORD;
+    databaseConnectionInfo.databaseUser = data.MARIADB_USERNAME;
+    databaseConnectionInfo.override = false;
+  }
+
+  return databaseConnectionInfo;
 }
 
 echo(`File reset for ${projectName} complete`);
