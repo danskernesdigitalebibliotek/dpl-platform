8f9c03d25121bd5890320ffedac2a2eaa335348b
ITViking <philip.dein@proton.me>
2025-06-04 11:49:39 +0200

    get all the sites from the sites.yaml file

--- infrastructure/task/scripts/delete-environment-level-variables.mjs
+++ infrastructure/task/scripts/delete-environment-level-variables.mjs
@@ -2,3 +2,5 @@
 
 echo(chalk.yellow("Deleting environment level environment variables"));
 echo("");
+const sites = await $`cat ../host_mount/environments/dplplat01/sites.yaml | yq '.sites | ... comments="" | keys | .[]'`;
+
