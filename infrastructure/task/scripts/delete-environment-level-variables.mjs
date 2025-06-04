eb1f282c44903257f0106ea09c700a5722050430
ITViking <philip.dein@proton.me>
2025-06-04 11:50:07 +0200

    list of variables we do not want to exist on the environment level

--- infrastructure/task/scripts/delete-environment-level-variables.mjs
+++ infrastructure/task/scripts/delete-environment-level-variables.mjs
@@ -4,3 +4,12 @@ echo(chalk.yellow("Deleting environment level environment variables"));
 echo("");
 const sites = await $`cat ../host_mount/environments/dplplat01/sites.yaml | yq '.sites | ... comments="" | keys | .[]'`;
 
+const lagoonVariableNames = [
+  "BNF_GRAPHQL_CONSUMER_SECRET",
+  "BNF_GRAPHQL_CONSUMER_USER_PASSWORD",
+  "GO_GRAPHQL_CONSUMER_SECRET",
+  "DRUPAL_REVALIDATE_SECRET",
+  "NEXT_PUBLIC_GO_GRAPHQL_CONSUMER_USER_PASSWORD",
+  "GO_SESSION_SECRET"
+];
+
