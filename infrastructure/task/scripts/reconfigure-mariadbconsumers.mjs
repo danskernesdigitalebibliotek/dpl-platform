669d751c8a4ede8f71eea4ae23d545fdc38edc67
ITViking <philip.dein@proton.me>
2025-09-17 14:01:38 +0200

    get the all the sites

--- infrastructure/task/scripts/reconfigure-mariadbconsumers.mjs
+++ infrastructure/task/scripts/reconfigure-mariadbconsumers.mjs
@@ -3,3 +3,5 @@
 echo(chalk.yellow("Will now reconfigure MariaDBConsumers"));
 echo("");
 
+const sites = await $`cat ../../../host_mount/environments/dplplat01/sites.yaml | yq '.sites | ... comments="" | keys | .[]'`;
+
