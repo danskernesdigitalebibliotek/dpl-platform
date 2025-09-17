d7e067f708376af422081d4e56b0d6847bdcbe9d
ITViking <philip.dein@proton.me>
2025-09-17 14:02:50 +0200

    make a blacklist of sites that can't be run with this script
    We have some sites that we used for testing. They have the same setup
    but some of the reources got named differently from the others. These
    sites therefore needs to be run manually

--- infrastructure/task/scripts/reconfigure-mariadbconsumers.mjs
+++ infrastructure/task/scripts/reconfigure-mariadbconsumers.mjs
@@ -5,3 +5,12 @@ echo("");
 
 const sites = await $`cat ../../../host_mount/environments/dplplat01/sites.yaml | yq '.sites | ... comments="" | keys | .[]'`;
 
+// These sites was setup differently than the production sites.
+const blackList = [
+  "customizable-canary",
+  "staging",
+  "canary",
+  "staging",
+  "bibliotek-test"
+];
+
