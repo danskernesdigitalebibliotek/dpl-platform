9c06a80d8d5ce45935a826da28433111964dbbb9
ITViking <philip.dein@proton.me>
2025-10-15 12:13:40 +0200

    upgrade lagoon-core to chart version 1.44.0

--- infrastructure/environments/dplplat01/lagoon/lagoon-core/upgrade-lagoon-core.sh
+++ infrastructure/environments/dplplat01/lagoon/lagoon-core/upgrade-lagoon-core.sh
@@ -3,4 +3,4 @@
 helm repo add lagoon https://uselagoon.github.io/lagoon-charts/
 helm repo update
 
-sops -d lagoon-core.values.sops.yaml | helm diff upgrade lagoon-core lagoon/lagoon-core --install -f - --version 1.41.0 --namespace lagoon-core --dry-run=server --debug
+sops -d lagoon-core.values.sops.yaml | helm diff upgrade lagoon-core lagoon/lagoon-core --install -f - --version 1.44.0 --namespace lagoon-core --dry-run=server --debug
