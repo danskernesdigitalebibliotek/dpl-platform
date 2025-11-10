489af2d08ec98b07af2d63448696621ffdf45649
ITViking <philip.dein@proton.me>
2025-11-10 14:57:02 +0100

    hardcode the path to sites yaml, as it seems to fix problme with running the site sync task

--- infrastructure/terraform/modules/dpl-platform-env-repos/variables.tf
+++ infrastructure/terraform/modules/dpl-platform-env-repos/variables.tf
@@ -17,7 +17,7 @@ variable "prefix" {
 locals {
   # Read in the list of sites from the root module, eg.
   # infrastructure/environments/dplplat01/sites.yaml
-  sites_config = yamldecode(file("${path.root}/../sites.yaml"))
+  sites_config = yamldecode(file("infrastructure/environments/dplplat01/sites.yaml"))
 
   # The "sites" entry in the file is an object with a key pr site we need to
   # create a repository for. The key is required to be a valid github project
