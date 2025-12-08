87f369bffb69504fe631009fc641e5467a815307
ITViking <philip.dein@proton.me>
2025-12-08 14:38:31 +0100

    ensure correct context and up the version number

--- infrastructure/environments/dplplat01/lagoon/lagoon-core/upgrade-lagoon-core.sh
+++ infrastructure/environments/dplplat01/lagoon/lagoon-core/upgrade-lagoon-core.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+kubectl config use-context aks-dplplat01-01
 helm repo add lagoon https://uselagoon.github.io/lagoon-charts/
 helm repo update
 
-sops -d lagoon-core.values.sops.yaml | helm diff upgrade lagoon-core lagoon/lagoon-core --install -f - --version 1.44.0 --namespace lagoon-core --dry-run=server --debug
+sops -d lagoon-core.values.sops.yaml | helm diff upgrade lagoon-core lagoon/lagoon-core --install -f - --version 1.45.0 --namespace lagoon-core --dry-run --debug
