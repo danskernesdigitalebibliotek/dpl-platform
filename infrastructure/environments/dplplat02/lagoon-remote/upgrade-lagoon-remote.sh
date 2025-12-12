59dba58bf13b4b668f05cb0024261b3d8de5adef
ITViking <philip.dein@proton.me>
2025-12-12 11:30:12 +0100

    ensure correct context and add important note on namespace for lagoon-remote

--- infrastructure/environments/dplplat02/lagoon-remote/upgrade-lagoon-remote.sh
+++ infrastructure/environments/dplplat02/lagoon-remote/upgrade-lagoon-remote.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+kubectl config use-context dplplat02
 helm repo add lagoon https://uselagoon.github.io/lagoon-charts/
 helm repo update
 
-sops -d values.sops.yaml | helm diff upgrade lagoon-remote lagoon/lagoon-remote --install -f - --version 0.89.0 --namespace lagoon --dry-run=server --debug
+# Lagoon remote must be installed into a namespace named "Lagoon": https://github.com/uselagoon/lagoon-charts/blob/main/charts/lagoon-remote/README.md#install
+sops -d values.sops.yaml | helm upgrade lagoon-remote lagoon/lagoon-remote --install -f - --version 0.89.0 --namespace lagoon --debug
