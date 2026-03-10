fe156c0c8bff4930377d7af139ce75952941566f
ITViking <philip.dein@proton.me>
2026-03-10 13:15:35 +0100

    change webhook url to the new one

--- infrastructure/environments/dplplat01/env_repos/main.tf
+++ infrastructure/environments/dplplat01/env_repos/main.tf
@@ -1,7 +1,7 @@
 module "env_repos" {
   source            = "../../../terraform/modules/dpl-platform-env-repos/"
   organisation_name = "dplplatinfra01"
-  webhook_url       = "https://webhookhandler.lagoon.dplplat01.dpl.reload.dk"
+  webhook_url       = "https://webhookhandler.lagoon.dplplat02.dpl.reload.dk"
 }
 
 output "site_repositories" {
